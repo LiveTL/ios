9a5e4c52fc897b9ea0d5d54194b37b980919177f
Candygoblen123 <andrew.glaze76@gmail.com>
2021-07-08 22:59:57 -0400

    set ios test target to ios 14.1

--- iosUnitTests/iosUnitTests.swift
+++ iosUnitTests/iosUnitTests.swift
@@ -22,7 +22,7 @@ class iosFilterTests: XCTestCase {
         let services = AppServices()
         services.settings.languages = TranslatedLanguageTag.allCases
             
-        let message1 = InjectedMessage(author: InjectedMessage.Author(id: "00", name: "Test", types: []), messages: [Message.text("(en) Hello There")], showtime: 0.0, timestamp: Date.now)
+        let message1 = InjectedMessage(author: InjectedMessage.Author(id: "00", name: "Test", types: []), messages: [Message.text("(en) Hello There")], showtime: 0.0, timestamp: Date.distantPast)
         if let tl1 = StreamModel(services).translate(message1) as? TranslatedMessage {
             XCTAssertEqual(tl1.languageTag, TranslatedLanguageTag.en)
             XCTAssertEqual(tl1.message, " Hello There")
@@ -30,7 +30,7 @@ class iosFilterTests: XCTestCase {
             XCTFail("(en) failed - not a translated message.")
         }
         
-        let message2 = InjectedMessage(author: InjectedMessage.Author(id: "00", name: "Test", types: []), messages: [Message.text("(es) - Hola eso")], showtime: 0.0, timestamp: Date.now)
+        let message2 = InjectedMessage(author: InjectedMessage.Author(id: "00", name: "Test", types: []), messages: [Message.text("(es) - Hola eso")], showtime: 0.0, timestamp: Date.distantPast)
         if let tl2 = StreamModel(AppServices()).translate(message2) as? TranslatedMessage {
             XCTAssertEqual(tl2.languageTag, TranslatedLanguageTag.es)
             XCTAssertEqual(tl2.message, " - Hola eso")
